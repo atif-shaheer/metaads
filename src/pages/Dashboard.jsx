a6d15b6e603b0be870ae278f758b0f8fd536aef5
Aatif Sahir <atifsha@outlook.com>
2025-10-10 22:15:19 +0530

    check

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -67,7 +67,9 @@ export default function Dashboard() {
   {/* Filters */}
   <div className="flex gap-3 px-1 mt-3">
     {/* Filter button */}
-    <button className="flex items-center gap-2 px-4 py-2 border bg-white text-base font-medium shadow-sm">
+    <button className="flex items-center gap-2 px-4 py-2 border bg-white"
+    style={{ fontSize: '1.2rem', lineHeight: '1.8rem' }}
+    >
       <svg
         xmlns="http://www.w3.org/2000/svg"
         className="w-5 h-5 text-gray-600"
